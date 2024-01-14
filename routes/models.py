f085e918176263a9dbcad944e976bd25f1ac8941
mpazaryna <mpazbot@gmail.com>
2024-01-14 14:16:29 -0500

    chore: added fema flood zone checker

--- routes/models.py
+++ routes/models.py
@@ -6,3 +6,8 @@ from pydantic import BaseModel
 class AddNumbers(BaseModel):
     number1: float
     number2: float
+
+
+class FloodZoneInquiry(BaseModel):
+    latitude: float
+    longitude: float
--- server.py
+++ server.py
@@ -3,9 +3,11 @@
 from fastapi import FastAPI
 
 from routes.add_routes import router as add_router
+from routes.fema_routes import router as fema_router
 from routes.root_routes import router as root_router
 
 app = FastAPI()
 
 app.include_router(add_router)
 app.include_router(root_router)
+app.include_router(fema_router)
